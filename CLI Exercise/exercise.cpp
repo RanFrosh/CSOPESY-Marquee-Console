a7c2917f6f119cc4072ccb1727a48d551b8ed862
Jus10-CYA <siajustin71@gmail.com>
2026-09-21 22:45:42 +0800

    fix: add space rejection functionality + text update for exercise

--- CLI Exercise/exercise.cpp
+++ CLI Exercise/exercise.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,9 +10,9 @@ int main() {
 
     cout << "==============================" << endl;
     cout << "     Main Menu Console" << endl;
-    cout << "==============================" << endl;
-    cout << "Group developer: Thea, Gerry, Philiipe" << endl;
-    cout << "Version date: September 18, 2026" << endl;
+    cout << "==============================" << endl;   
+    cout << "Group developer: \nChu, Avery\nSia, Justin\nTan, Paul\nTiu, Avram\n" << endl;
+    cout << "Version date: September 21, 2026" << endl;
     cout << endl;
 
     // Command loop
@@ -23,14 +24,25 @@ int main() {
             cout << "help - Displays the available commands." << endl;
             cout << "set_text <your_string> - Sets the text for the marquee." << endl;
             cout << "exit - Terminates the console." << endl;
+			cout << "NOTE: These commands below won't work for this exercise" << endl;
+			cout << "start_marquee - Starts the marquee animation." << endl;
+			cout << "stop_marquee - Stops the marquee animation." << endl;
+			cout << "set_speed <speed_value> - Sets the speed of the marquee animation." << endl;
         }
         else if (command.rfind("set_text ", 0) == 0) {
             marqueeText = command.substr(9);
-            if(!marqueeText.empty()) {
-                cout << "Text accepted for marquee: " << marqueeText << endl; 
-            } else {
+            string nospace = command;
+            nospace.erase(remove(nospace.begin(), nospace.end(), ' '), nospace.end());
+
+            if (!marqueeText.empty() && nospace.length() > 8) {
+                cout << "Text accepted for marquee: " << marqueeText << endl;
+            }
+            else {
                 cout << "Error: No text provided for marquee." << endl;
             }
+        }
+        else if (command == "exit") {
+            exit(0);
         }
         else {
             cout << "Error: Unrecognized command." << endl;
